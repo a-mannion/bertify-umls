23d4b6607a9e3d7275271410af731c4a2e4974e2
manniona <aidan.mannion@univ-grenoble-alpes.fr>
2024-02-08 10:57:09 +0100

    more complete cl argument options: gradient tracking with wandb, learning rate warmup options; also type hints

--- training_scripts/train.py
+++ training_scripts/train.py
@@ -6,7 +6,8 @@ import json
 import logging
 from functools import partial
 from datetime import datetime
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from typing import Union
 
 import torch
 import wandb
@@ -27,7 +28,6 @@ here, _ = os.path.split(os.path.realpath(__file__))
 sys.path.append(os.path.normpath(os.path.join(here, os.pardir, "src")))
 from kgi_bert import KgiLMBert
 from data_utils import (
-    Bunch,
     get_relation_labels,
     prepare_mixed_dataset,
     mixed_collate_fn
@@ -48,15 +48,23 @@ i.e. randomly initialise the weights to train from scratch"""
 ABST_HELP = """Manually add padding, masking, and separation tokens to the tokenizer during 
 data preprocessing"""
 MODEL_RUN_NAME_HELP = """Name to use for the output directory"""
-CONFIG_FILE_HELP = """Path to a json file containing additional arguments, see `config_files/kgi_config.json`"""
+TRAINSETFRAC_HELP = "Proportion of the input dataset to use for training"
+BATCH_SIZE_HELP = "Number of samples to process at a time"
+LR_HELP = "Learning rate to use for optimisation"
+EPOCHS_HELP = "Number of passes to run over the training set"
+GRAD_ACC_HELP = "Number of batches over which to add up gradients between each backward pass"
+LIN_SCHED_EPOCH_HELP = """For cases in which a non-linear learning rate schedule is being spread out
+over multiple runs of this script, use this flag to indicate how many epochs have already been run"""
+SEQ_LEN_HELP = "Maximal number of tokens per training sequence"
+SEED_HELP = "Random seed to use for initialisation"
+TASK_COEFS_HELP = """Floating-point weights for the loss function components corresponding to each KG-based
+task; expects three numbers, the first for entity prediction, the second for link prediction and the third
+for triple classification"""
+CP_INTERVAL_HELP = "Save a checkpoint every n epochs"
 N_TEXT_DOCS_HELP = """Specify a maximum number of documents to load from the `corpus_fp` file; leave unspecified
 to use all of them"""
-EXCLUDE_TASK_HELP = """Integer argument telling the model to ignore one of the training objectives (for ablation
-experiments) - 0 for entity prediction, 1 for link prediction and 2 for triple classification"""
 FP16_HELP = """Use 16-bit precision training"""
 CONST_SCHED_HELP = """Use a constant learning rate"""
-AUTOCOEF_HELP = """Automatically weight the loss function components for each sub-task based on the number of 
-available training examples for each one - takes precedence over the values specified in `config_file`"""
 TDO_HELP = """Only update model weights based on the training data - otherwise, the model will  be trained on the
 validation set AFTER the standard training run; use this flag when you intend to continue training the model on
 the same dataset later"""
@@ -64,9 +72,11 @@ NOSAVE_HELP = """Doesn't write anything to disk - can be useful for debugging"""
 PROJ_HELP = """Name of the Weights & Biases project to log progress to"""
 ENT_HELP = """Your Weights & Biases username; if this and/or `wandb_proj` are not provided, the script will run
 locally without logging metrics"""
+LOGFREQ_HELP = "Log metrics to W&B after every n batches; if not specified will update after each backward pass"
+TRACKGRAD_HELP = "Log the gradients of the model to Weights & Biases as well as the performance metrics"
 
 
-def parse_arguments():
+def parse_arguments() -> Namespace:
     """Command line arguments"""
     parser = ArgumentParser()
     parser.add_argument("kg_fp", type=str, help=KG_FP_HELP)
@@ -77,30 +87,29 @@ def parse_arguments():
     parser.add_argument("--from_scratch", action="store_true", help=FROM_SCRATCH_HELP)
     parser.add_argument("--add_bert_special_tokens", action="store_true", help=ABST_HELP)
     parser.add_argument("--model_run_name", type=str, default="kgi", help=MODEL_RUN_NAME_HELP)
-    default_config_file = os.path.join(here, "config_files/kgi_config.json")
-    parser.add_argument("--config_file", type=str, default=default_config_file, help=CONFIG_FILE_HELP)
+    parser.add_argument("--train_set_frac", type=float, default=.95, help=TRAINSETFRAC_HELP)
+    parser.add_argument("--batch_size", type=int, default=16, help=BATCH_SIZE_HELP)
+    parser.add_argument("--lr", type=float, default=.00002, help=LR_HELP)
+    parser.add_argument("--epochs", type=int, default=4, help=EPOCHS_HELP)
+    parser.add_argument("--grad_acc", type=int, default=4, help=GRAD_ACC_HELP)
+    parser.add_argument("--linear_schedule_epochs", type=int, help=LIN_SCHED_EPOCH_HELP)
+    parser.add_argument("--seq_len", type=int, default=128, help=SEQ_LEN_HELP)
+    parser.add_argument("--seed", type=int, default=42, help=SEED_HELP)
+    parser.add_argument("--task_coefs", type=float, nargs=3, help=TASK_COEFS_HELP)
+    parser.add_argument("--epoch_checkpoint_interval", type=int, default=4, help=CP_INTERVAL_HELP)
     parser.add_argument("--n_text_docs", type=int, help=N_TEXT_DOCS_HELP)
-    parser.add_argument("--exclude_task", type=int, choices={0, 1, 2}, help=EXCLUDE_TASK_HELP)
     parser.add_argument("--fp16", action="store_true", help=FP16_HELP)
     parser.add_argument("--constant_schedule", action="store_true", help=CONST_SCHED_HELP)
-    parser.add_argument("--auto_coef", action="store_true", help=AUTOCOEF_HELP)
     parser.add_argument("--train_data_only", action="store_true", help=TDO_HELP)
     parser.add_argument("--nosave", action="store_true", help=NOSAVE_HELP)
     parser.add_argument("--wandb_proj", type=str, help=PROJ_HELP)
     parser.add_argument("--wandb_entity", type=str, help=ENT_HELP)
+    parser.add_argument("--wandb_log_freq", type=int, help=LOGFREQ_HELP)
+    parser.add_argument("--track_grad", action="store_true", help=TRACKGRAD_HELP)
     return parser.parse_args()
 
 
-def setup_metric_step_axes():
-    # define x-axis for graphs
-    wandb.define_metric("train_step")
-    wandb.define_metric("eval_step")
-    # associate output metrics with the corresponding x-axis metric
-    wandb.define_metric("train_loss", step_metric="train_step")
-    wandb.define_metric("eval_loss", step_metric="eval_step")
-
-
-def run_pipeline(config):
+def run_pipeline(config: Union[Namespace, wandb.sdk.Config], logger: logging.Logger) -> None:
     # setup
     accelerator = Accelerator(
         mixed_precision="fp16" if config.fp16 else None,
@@ -146,40 +155,38 @@ def run_pipeline(config):
         model_config = AutoConfig.from_pretrained(config.model_path, vocab_size=vocab_size)
         
         num_labels_link_pred = len(rel_token_ids2labels)
-        if config.auto_coef:
+        if not config.task_coefs:
             _, task_idx_counts = np.unique(train_dataset.task_type_index, return_counts=True)
             kg_task_idx_counts = task_idx_counts[:-1]
             kg_task_idx_counts_sum = kg_task_idx_counts.sum()
             task_weight_coefficients = (kg_task_idx_counts_sum - kg_task_idx_counts) /\
                 (2 * kg_task_idx_counts_sum)
             task_weight_coefficients[kg_task_idx_counts == 0] = 0
-        else:
-            task_weight_coefficients = np.fromiter(
-                (config.__dict__["task_coef" + str(i)] for i in range(3)),
-                dtype=np.float32
-            )
-        if config.exclude_task is not None:
-            # in case the sequences corresponding to the task to be ignored have not been
-            # excluded from the input dataset
-            task_weight_coefficients[config.exclude_task] = 0
+            config.task_coefs = task_weight_coefficients.tolist()
+        
         model = KgiLMBert(
             model_config,
             from_pretrained=config.model_path if not config.from_scratch else None,
             num_labels_link_pred=num_labels_link_pred,
-            task_weight_coefficients=task_weight_coefficients.tolist()
+            task_weight_coefficients=config.task_coefs
         )
-    optimizer = AdamW(tuple(model.parameters()), lr=config.learning_rate)
+    optimizer = AdamW(tuple(model.parameters()), lr=config.lr)
     if config.from_cp:
         optimizer_state_dict = torch.load(os.path.join(config.model_path, "optimizer.bin"))
         optimizer.load_state_dict(optimizer_state_dict)
 
-    now = datetime.now()
-    output_subdir = f"{config.model_run_name}_{now.day}-{now.month}_{now.hour}-{now.minute}"
-    output_fp = os.path.join(os.getenv("HOME"), "mbiolm-proj/kgi-runs", output_subdir)
-    if not os.path.isdir(output_fp):
-        os.mkdir(output_fp)
-    with open(os.path.join(output_fp, "script_params.json"), "w", encoding=TEXT_ENC) as f_io:
-        json.dump(config.as_dict(), f_io)
+    if not config.nosave:
+        now = datetime.now()
+        output_subdir = f"{config.model_run_name}_{now.day}-{now.month}_{now.hour}-{now.minute}"
+        output_fp = os.path.join(os.getenv("HOME"), "mbiolm-proj/kgi-runs", output_subdir)
+        if not os.path.isdir(output_fp):
+            os.mkdir(output_fp)
+        try:
+            script_params = config.as_dict()
+        except AttributeError:
+            script_params = vars(config)
+        with open(os.path.join(output_fp, "script_params.json"), "w", encoding=TEXT_ENC) as f_io:
+            json.dump(script_params, f_io)
 
     n_train = len(train_dataset)
     n_train_batches = len(train_dataloader)
@@ -237,10 +244,25 @@ def run_pipeline(config):
 
     wandb_active = isinstance(config, wandb.sdk.wandb_config.Config)
     if wandb_active:
-        wandb.watch(model, log_freq=config.wandb_log_freq)
-        setup_metric_step_axes()
+        if config.track_grad:
+            wandb.watch(model, log_freq=config.wandb_log_freq if config.wandb_log_freq else 200)
+        
+        # define x-axis for graphs
+        wandb.define_metric("train_step")
+        wandb.define_metric("eval_step")
+        # associate output metrics with the corresponding x-axis metric
+        wandb.define_metric("train_loss", step_metric="train_step")
+        wandb.define_metric("eval_loss", step_metric="eval_step")
+
+        train_step, eval_step = 0, 0  # update counters
+
+        # log function that takes the update counter and loss tensor
+        def wandb_log(loss, train_step):
+            loss_ = loss if isinstance(loss, float) else loss.detach().item()
+            metrics_to_log = {"train_step": train_step, "train_loss": loss_}
+            wandb.log(metrics_to_log)
+
     eval_loss_list = []
-    n_examples_train, n_examples_eval = 0, 0
     for epoch in range(config.epochs):
         model.train()
         if wandb_active:
@@ -250,14 +272,17 @@ def run_pipeline(config):
                 outputs = model(**batch)
                 loss = outputs.loss
                 accelerator.backward(loss)
-                n_examples_train += config.batch_size
                 optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
-            if wandb_active and idx % config.wandb_log_freq == 0:
-                loss_ = loss.detach().item()
-                metrics_to_log = {"train_step": n_examples_train, "train_loss": loss_}
-                wandb.log(metrics_to_log)
+            if wandb_active:
+                if config.wandb_log_freq is None:
+                    if idx % config.grad_acc == 0 or idx == n_train_batches - 1:
+                        train_step += 1
+                        wandb_log(loss, train_step)
+                elif idx % config.wandb_log_freq == 0:
+                    train_step += 1
+                    wandb_log(loss, train_step)
 
         model.eval()
         batch_eval_loss = []
@@ -265,19 +290,23 @@ def run_pipeline(config):
             with torch.no_grad():
                 outputs = model(**batch)
             loss_val = outputs.loss.detach().item()
+            batch_eval_loss.append(loss_val)
             if np.isnan(loss_val):  # usually underflow afaik
                 loss_val = 1e-6
-            n_examples_eval += config.batch_size
-            if wandb_active and idx % config.wandb_log_freq == 0:
-                metrics_to_log = {"eval_step": n_examples_eval, "eval_loss": loss_val}
-                wandb.log(metrics_to_log)
-            batch_eval_loss.append(loss_val)
+            if wandb_active:
+                if config.wandb_log_freq is None:
+                    if idx % config.grad_acc == 0 or idx == n_train_batches - 1:
+                        eval_step += 1
+                        wandb_log(loss_val, eval_step)
+                elif idx % config.wandb_log_freq == 0:
+                    eval_step += 1
+                    wandb_log(loss_val, eval_step)
         epoch_eval_loss = sum(batch_eval_loss) / len(batch_eval_loss)
         eval_loss_list.append(epoch_eval_loss)
 
         logger.info(
-            "\tFinished epoch %d - eval. loss = %d...",
-            epoch, round(epoch_eval_loss, 3)
+            "\tFinished epoch %d - eval. loss = %.5f...",
+            epoch + 1, epoch_eval_loss
         )
         if (epoch + 1) % config.epoch_checkpoint_interval == 0 and epoch != config.epochs - 1:
             save_checkpoint()
@@ -307,24 +336,18 @@ def run_pipeline(config):
     logger.info("Done!")
 
 
-def main(args):
-    # config setup
-    with open(args.config_file, encoding=TEXT_ENC) as f_io:
-        config = {**vars(args), **json.load(f_io)}
-
+def main(args: Namespace, logger: logging.Logger) -> None:
     if args.wandb_proj and args.wandb_entity:
-        with wandb.init(project=args.wandb_proj, entity=args.wandb_entity, config=config):
-            config = wandb.config
+        with wandb.init(project=args.wandb_proj, entity=args.wandb_entity, config=vars(args)):
             logger.info("Launching training with W&B...")
-            run_pipeline(config)
+            run_pipeline(wandb.config, logger)
     else:
         logger.info("Launching training...")
-        config = Bunch(**config)
-        run_pipeline(config)
+        run_pipeline(args, logger)
 
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     logging.basicConfig(format=LOGFMT, datefmt="%d/%m/%Y %H:%M:%S", level=logging.INFO)
-    main(parse_arguments())
+    main(parse_arguments(), logger)
     
